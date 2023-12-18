7f19e41c326f1d94cc05a8e11e37a86ad630bdca
karaneliab <eliabkaran7884@gmail.com>
2023-12-18 13:32:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+const newArray =[];
+
+
+function writeCards(names,event){
+    newArray.length = 0;
+    for (let i = 0; i < names.length; i++){
+        let Cdmessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray.push(Cdmessage);
+    
+    }
+    return newArray;
+}
+const result = writeCards(names,event);
+console.log(result);
+
+function countDown(){
+    let startingNumber = 10;
+    while(startingNumber >= 0){
+        console.log(startingNumber);
+        startingNumber --;
+
+    }
+}
+countDown(10);
